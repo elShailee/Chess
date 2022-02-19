ff8b5b7d56933065c2f79f44fdebffa03a4071d9
Shailee Eliyahu <shailee.eliyahu@gmail.com>
2022-02-19 15:43:05 +0200

    basic render of pieces sprites

---
+++ front/src/Assets/pieces/lib.js
@@ -0,0 +1,31 @@
+import blackBishop from './b_bishop.svg';
+import blackKing from './b_king.svg';
+import blackKnight from './b_knight.svg';
+import blackPawn from './b_pawn.svg';
+import blackQueen from './b_queen.svg';
+import blackRook from './b_rook.svg';
+import whiteBishop from './w_bishop.svg';
+import whiteKing from './w_king.svg';
+import whiteKnight from './w_knight.svg';
+import whitePawn from './w_pawn.svg';
+import whiteQueen from './w_queen.svg';
+import whiteRook from './w_rook.svg';
+
+export const lib = {
+	black: {
+		bishop: blackBishop,
+		king: blackKing,
+		knight: blackKnight,
+		pawn: blackPawn,
+		queen: blackQueen,
+		rook: blackRook,
+	},
+	white: {
+		bishop: whiteBishop,
+		king: whiteKing,
+		knight: whiteKnight,
+		pawn: whitePawn,
+		queen: whiteQueen,
+		rook: whiteRook,
+	},
+};
--- front/src/Screens/Gameplay/Tile.jsx
+++ front/src/Screens/Gameplay/Tile.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { StyledTile } from './styles';
+import { lib } from 'Assets/pieces/lib';
 
 export default function Tile({ x, y, tileColor, chessPiece }) {
-	return (
-		<StyledTile tileColor={tileColor}>{chessPiece ? chessPiece.color + ' ' + chessPiece.type : x + ' ' + y}</StyledTile>
-	);
+	return <StyledTile tileColor={tileColor}>{chessPiece && <img src={lib[chessPiece.color][chessPiece.type]} />}</StyledTile>;
 }
