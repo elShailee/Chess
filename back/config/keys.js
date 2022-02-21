a58a56073fa986a0752ca195405388bb835d7ea1
Dort <dortam888@gmail.com>
2022-02-21 09:32:18 +0200

    Auth
    Auth Process of Login and Logout

---
+++ back/config/keys.js
@@ -0,0 +1,9 @@
+module.exports = 
+{
+	googleClientID: '375339275853-fp86nhppgdkbuc3tbel5im2kefkcdec1.apps.googleusercontent.com',
+	googleClientSecret: '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',
+	facebookClientID: '4721329861319113',
+	facebookClientSecret: 'd86b4e294cc3c04ea2ccb2180ba036cf',
+	mongoURI: 'mongodb+srv://dortam888:7XR9FUZXnQn2sFz@cluster0.o7rsy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+	cookieKey: 'redacted'
+};
